39c054958789619d575c30b1c7839e7b0e125faf
Nicklas Utgaard <nicklas.utgaard@bekk.no>
2017-04-03 09:25:31 +0200

    Custom menu

--- src/application.js
+++ src/application.js
@@ -5,11 +5,26 @@ import Footer from './components/footer/footer';
 
 import Github from './components/github/github';
 import About from './components/about/about';
+import Clipper from './components/clipper/clipper';
 
-function Application(){
+function Application() {
     return (
         <div className="application">
             <Header />
+            <div className="menu" style={{ margin: '1rem', marginTop: 0 }}>
+                <Clipper Tag="a" className="test" href="http://www.utgaard.xyz">
+                    A menu
+                </Clipper>
+                <Clipper Tag="a" className="test" href="http://www.utgaard.xyz">
+                    concept
+                </Clipper>
+                <Clipper Tag="a" className="test" href="http://www.utgaard.xyz">
+                    which is
+                </Clipper>
+                <Clipper Tag="a" className="test" href="http://www.utgaard.xyz">
+                    perhaps usable
+                </Clipper>
+            </div>
             <main className="application__main">
                 <Tabs tabTitles={['Github', 'About']}>
                     <Github />
