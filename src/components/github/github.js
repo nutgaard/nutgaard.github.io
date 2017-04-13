23eab373d27c7869d41df9a016eac5d0af5782ed
Nicklas Utgaard <nicklas.utgaard@bekk.no>
2017-04-13 15:42:12 +0200

    Added github statistics and used the grid-component

--- src/components/github/github.js
+++ src/components/github/github.js
@@ -1,6 +1,12 @@
 import React, { Component, PropTypes as PT } from 'react';
+import Grid from './../grid/grid';
 import Async from './../async/async';
+import Softbreak from './../softbreak';
 import './github.css';
+
+function maxBy(list, property) {
+    return list.reduce((maxElement, element) => maxElement[property] > element[property] ? maxElement : element, { [property]: -Infinity });
+}
 
 function GithubRepo({ repo }) {
     if (repo.has_pages) {
@@ -14,9 +20,30 @@ function GithubRepo({ repo }) {
     }
     return (
         <div className="github__repo">
-            <h3>{repo.name}</h3>
+            <Softbreak tag="h3">{repo.name}</Softbreak>
             <p>{repo.description}</p>
         </div>
+    );
+}
+
+function GithubStatistics({ repos }) {
+    const NOF_repos = repos.length;
+
+    const lastUpdated = repos[0] ? repos[0].name : 'Unknown';
+    const mostWatchers = maxBy(repos, 'watchers_count');
+    const mostForks = maxBy(repos.filter((repo) => !repo.fork), 'forks_count');
+    const mostIssues = maxBy(repos, 'open_issues_count');
+    const mostStars = maxBy(repos, 'stargazers_count');
+
+    return (
+        <Grid className="github__statistics" large="3">
+            <p><b>Number of repositories: </b>{NOF_repos}</p>
+            <p><b>Latest update: </b>{lastUpdated}</p>
+            <p><b>Most watchers: </b>{mostWatchers.name} ({mostWatchers.watchers_count})</p>
+            <p><b>Most forked: </b>{mostForks.name} ({mostForks.forks_count})</p>
+            <p><b>Most open issues: </b>{mostIssues.name} ({mostIssues.open_issues_count})</p>
+            <p><b>Most stars: </b>{mostStars.name} ({mostStars.stargazers_count})</p>
+        </Grid>
     );
 }
 
@@ -32,9 +59,9 @@ class Github extends Component {
 
     componentDidMount() {
         // Fixture for development...
+        //const data = require('./../../fixtures/github_repos.json');
+        //this.setState({ isLoading: false, data });
         //setTimeout(() => {
-        //    const data = require('./../../fixtures/github_repos.json');
-        //    this.setState({ isLoading: false, data });
         //}, 1000);
 
         // Real integration
@@ -49,7 +76,6 @@ class Github extends Component {
                 {() => {
                     const repos = this.state.data
                         .map((repo) => ({ repo, date: new Date(repo.pushed_at)}))
-                        .filter(({date}) => date.getFullYear() >= 2016)
                         .sort((a, b) => {
                             if (a.repo.has_pages && b.repo.has_pages) {
                                 return (new Date(a.date) > new Date(b.date) ? -1 : 1);
@@ -60,11 +86,16 @@ class Github extends Component {
                             }
                             return new Date(a.date) > new Date(b.date) ? -1 : 1;
                         })
-                        .map(({repo}) => <GithubRepo key={repo.name} repo={repo} />);
+                        .map(({ repo }) => repo);
+                    const repoElements = repos
+                        .map((repo) => <GithubRepo key={repo.name} repo={repo} />);
 
                     return (
                         <div className="github">
-                            {repos}
+                            <GithubStatistics repos={repos} />
+                            <Grid className="github__repos" large="3" padToMatch={<div className="github__emptyrepo" />}>
+                                {repoElements}
+                            </Grid>
                         </div>
                     );
                 }}
