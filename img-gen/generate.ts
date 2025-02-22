5f787973af09d84d06a28bb276c59c6c5f19797b
Nicklas Utgaard <nutgaard@gmail.com>
2025-02-22 21:24:19 +0100

    add summary to imggen workflow

--- img-gen/generate.ts
+++ img-gen/generate.ts
@@ -1,86 +1,68 @@
-import puppeteer, {Page} from 'puppeteer';
-import {fetchRepos, GithubRepo} from '@/resources/github';
+import puppeteer, {Browser, Page} from 'puppeteer';
 import {del, list, put} from '@vercel/blob';
-import * as paths from 'node:path';
-import os from "node:os";
+import {fetchRepos} from '@/resources/github';
+import {addSummaryToOutput, analyze, needsBrowser, screenshot, SummaryAction} from "./processes";
 
-const tmpdir = os.tmpdir();
-const repos = await fetchRepos();
-const pages = repos.filter(it => it.has_pages);
-console.log(`Found ${repos.length} repositories, ${pages.length} with pages`);
+let browser: Browser | undefined = undefined;
+let page: Page;
 
-const browser = await puppeteer.launch({
-    headless: true,
-    args: [
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-    ]
-});
-const page = await browser.newPage();
+const repositories = await fetchRepos();
+const pages = repositories.filter(it => it.has_pages);
+console.log(`Found ${repositories.length} repositories, ${pages.length} with pages`);
 
 const blobsStorage = await list({ limit: pages.length + 10 });
 console.log(`Found ${blobsStorage.blobs.length} blobs, (hasMore: ${blobsStorage.hasMore})`);
 
-const missingImage = blobsStorage.blobs.find(it => it.pathname == '404.svg');
-if (!missingImage) {
-    await put(`404.svg`, Bun.file('./img-gen/missing.svg').stream(), { access: 'public' });
+const actionsToTake = analyze(repositories, blobsStorage.blobs);
+const startBrowser = actionsToTake.some(needsBrowser);
+if (startBrowser) {
+    console.log('Starting browser...');
+    browser = await puppeteer.launch({
+        headless: true,
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+        ]
+    });
+    page = await browser.newPage();
 }
 
 let i = 1;
-for (const repo of repos) {
-    console.log(`[${(i++).toString().padStart(3, '0')}] Processing ${repo.name}`);
-    const filename = `${repo.name}.png`;
-    const lastUpdateToRepo = new Date(repo.updated_at);
+const summary: SummaryAction[] = [];
 
-    const blob = blobsStorage.blobs.find((blob) => {
-        return blob.pathname == filename
-    });
+const missingImage = blobsStorage.blobs.find(it => it.pathname == '404.svg');
+if (!missingImage) {
+    summary.push({ name: '404.svg', action: 'CREATE' });
+    await put(`404.svg`, Bun.file('./img-gen/missing.svg').stream(), { access: 'public' });
+}
 
-    if (blob) {
-        if (!repo.has_pages) {
-            console.log('\tMissing image already uploaded.')
-        } else if (blob.uploadedAt < lastUpdateToRepo) {
-            console.log(`\tScreenshotting ${repo.name} because of updated repository.`);
-            const { filename, filepath } = await screenshot(page, repo);
-            console.log(`\tUploading ${filename}.`);
-            await put(filename, Bun.file(filepath).stream(), { access: 'public' });
-            console.log(`\tDeleting old image ${blob.url}`);
-            await del(blob.url)
-        } else {
-            console.log('\tImage of GH-pages already updated.')
-        }
+for (const actionToTake of actionsToTake) {
+    const { repository, action } = actionToTake;
+    console.log(`[${(i++).toString().padStart(3, '0')}] Processing ${repository.name}`);
+    summary.push({ name: repository.name, action });
+
+    if (action == 'DELETE') {
+        console.log(`\tDeleting old image ${actionToTake.blob.url}`);
+        await del(actionToTake.blob.url);
+    } else if (action == 'CREATE') {
+        console.log(`\tScreenshotting ${repository.name} because image was missing.`);
+        const { filename, filepath } = await screenshot(page!!, repository);
+        console.log(`\tUploading ${filename}.`);
+        await put(filename, Bun.file(filepath).stream(), { access: 'public' });
+    } else if (action == 'UPDATE') {
+        console.log(`\tScreenshotting ${repository.name} because of updated repository.`);
+        const { filename, filepath } = await screenshot(page!!, repository);
+        console.log(`\tUploading ${filename}.`);
+        await put(filename, Bun.file(filepath).stream(), { access: 'public' });
+        console.log(`\tDeleting old image ${actionToTake.blob.url}`);
+        await del(actionToTake.blob.url)
     } else {
-        if (repo.has_pages) {
-            console.log(`\tScreenshotting ${repo.name} because image was missing.`);
-            const { filename, filepath } = await screenshot(page, repo);
-            console.log(`\tUploading ${filename}.`);
-            await put(filename, Bun.file(filepath).stream(), { access: 'public' });
-        } else {
-            console.log(`\tSkipping, it will use 404.svg.`);
-        }
+        console.log(`\tSkipping ${repository.name}. ${actionToTake.ignoreReason}`);
     }
 }
 
-await browser.close();
-
-type Screenshot = {
-    filename: string;
-    filepath: string;
+if (browser) {
+    await browser.close();
 }
-async function screenshot(page: Page, repo: GithubRepo): Promise<Screenshot> {
-    if (repo.name == 'nutgaard.github.io') {
-        await page.goto(`https://www.utgaard.xyz/pages`);
-    } else {
-        await page.goto(`https://github.utgaard.xyz/${repo.name}`);
-    }
 
-    await page.setViewport({width: 1024, height: 1024});
-
-    const filename = `${repo.name}.png`;
-    const filepath = paths.join(tmpdir, filename);
-    await page.screenshot({
-        path: filepath
-    });
-
-    return { filename, filepath }
-}
+await addSummaryToOutput(summary);
---
+++ img-gen/processes.ts
@@ -0,0 +1,117 @@
+import * as core from '@actions/core';
+import {GithubRepo} from "@/resources/github";
+import {ListBlobResultBlob} from "@vercel/blob";
+import {Page} from "puppeteer";
+import paths from "node:path";
+import os from "node:os";
+
+export type Action = IgnoreAction | DeleteAction | UpdateAction | CreateAction;
+export type ActionType = Action['action'];
+type IgnoreAction = {
+    action: 'IGNORE';
+    repository: GithubRepo;
+    ignoreReason: string;
+}
+type DeleteAction = {
+    repository: GithubRepo;
+    action: 'DELETE';
+    blob: ListBlobResultBlob;
+}
+type UpdateAction = {
+    repository: GithubRepo;
+    action: 'UPDATE';
+    blob: ListBlobResultBlob;
+}
+type CreateAction = {
+    repository: GithubRepo;
+    action: 'CREATE';
+}
+export function needsBrowser({action}: Action): boolean {
+    return action == 'UPDATE' || action == 'CREATE';
+}
+
+
+export function analyze(
+    repositories: GithubRepo[],
+    blobs: ListBlobResultBlob[]
+): Action[] {
+    const actions: Action[] = [];
+    const blobLookup = blobs.reduce((acc, blob) => {
+        acc[blob.pathname] = blob;
+        return acc;
+    }, {} as Record<string, ListBlobResultBlob>);
+
+    for (const repository of repositories) {
+        const expectedFilename = `${repository.name}.png`;
+        const blob = blobLookup[expectedFilename];
+        let action: ActionType;
+
+        if (!blob) {
+            action = repository.has_pages ? 'CREATE' : 'IGNORE'
+            actions.push({ repository, action, ignoreReason: 'It will use 404.svg' });
+        } else if (!repository.has_pages) {
+            action = 'DELETE'
+            actions.push({ repository, blob, action });
+        } else {
+            action = blob.uploadedAt < new Date(repository.updated_at) ? 'UPDATE' : 'IGNORE'
+            actions.push({ repository, blob, action, ignoreReason: 'Already up to to date.' });
+        }
+    }
+
+    return actions;
+}
+
+export type SummaryAction = { name: string; action: ActionType };
+export async function addSummaryToOutput(actions: SummaryAction[]) {
+    const actionCount = actions.reduce((acc, action) => {
+        const count = acc[action.action] ?? 0;
+        acc[action.action] = count + 1;
+        return acc;
+    }, {} as Record<ActionType, number>);
+
+    core.summary.addHeading('Summary');
+    core.summary.addTable([
+        [header('Action'), header('Count')],
+        ...Object.entries(mapValues(actionCount, it => it.toString()))
+    ]);
+
+    core.summary.addBreak();
+    core.summary.addHeading('Actions taken');
+    core.summary.addTable([
+        [header('Repository'), header('Action')],
+        ...actions.map((it) => [it.name, it.action])
+    ]);
+
+    await core.summary.write();
+}
+
+const header = (data: string) => ({ data, header: true })
+const mapValues = <S extends string, T, V>(record: Record<S, T>, fn: (value: T) => V): Record<S, V> => {
+    return Object.fromEntries(
+        Object.entries(record).map(([key, value]) => [key, fn(value as T)])
+    ) as Record<S, V>;
+}
+
+export type Screenshot = {
+    filename: string;
+    filepath: string;
+}
+
+const tmpdir = os.tmpdir();
+export async function screenshot(page: Page, repo: GithubRepo): Promise<Screenshot> {
+    if (repo.name == 'nutgaard.github.io') {
+        await page.goto(`https://www.utgaard.xyz/pages`);
+    } else {
+        await page.goto(`https://github.utgaard.xyz/${repo.name}`);
+    }
+
+    await page.setViewport({width: 1024, height: 1024});
+
+    const filename = `${repo.name}.png`;
+    const filepath = paths.join(tmpdir, filename);
+    await page.screenshot({
+        path: filepath
+    });
+
+    return { filename, filepath }
+}
